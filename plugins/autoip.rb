285ccac2c7b943d3d311a4390ce4f7f3765c9bc1
Doug Morris <dmorris@covermymeds.com>
2016-05-06 15:58:45 -0400

    updates to use phpIPAM api for IP requests

--- plugins/autoip.rb
+++ plugins/autoip.rb
@@ -2,6 +2,7 @@
 
 require "net/https"
 require "uri"
+require "json"
 require "ipaddr"
 
 class Autoip < Plugin
@@ -20,6 +21,9 @@ class Autoip < Plugin
     opts.on( '--apitoken apitoken', "Token to use with the api application (#{options[:apitoken]})") do |x|
       options[:apitoken] = x
     end
+#    opts.on( '-p', '--password PASSWORD', 'vSphere password') do |x|
+#      options[:password] = x
+#    end
     return opts, options
   end
 
@@ -35,7 +39,82 @@ class Autoip < Plugin
       # Remove any 'DOMAIN\' prefix from the username
       username = options[:username]
       username = username.gsub(/^.+\\(.*)/, '\1')
+      if (not options[:password])
+        print 'Password: '
+        options[:password] = STDIN.noecho(&:gets).chomp
+        puts ''
+      end
       puts "Requesting IP in #{options[:subnet]} vlan"
+puts username
+
+      uri = options[:add_uri].gsub(/APIAPP/, {
+        'APIAPP' => options[:apiapp],
+      })
+      uri = uri 
+      uri = URI.escape(uri)
+      uri = URI.parse(uri.concat("user/"))
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request.basic_auth username, options[:password]
+      response = http.request(request)
+      if response.code != "200"
+        abort "There was an error requesting your IP address, IPAM returned code: #{response.code}, message: #{response.body}"
+      end
+      response_hash = JSON.parse(response.body)
+      puts response_hash["code"]
+      puts response_hash["data"]["token"]
+      auth_token = response_hash["data"]["token"]
+
+      uri = options[:add_uri]
+      uri = URI.escape(uri)
+      uri = URI.parse(uri.concat("subnets/cidr/#{options[:subnet]}"))
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri.request_uri)
+      request.add_field("token",  auth_token)
+      response = http.request(request)
+      if response.code != "200"
+        abort "There was an error requesting your IP address, IPAM returned code: #{response.code}, message: #{response.body}"
+      end
+      response_hash = JSON.parse(response.body)
+      puts response_hash["code"]
+      puts response_hash["data"][0]["id"]
+      subnetId = response_hash["data"][0]["id"]
+
+      uri = options[:add_uri]
+      uri = URI.escape(uri)
+      uri = URI.parse(uri.concat("subnets/#{subnetId}/first_free/"))
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Get.new(uri.request_uri)
+      request.add_field("token",  auth_token)
+      response = http.request(request)
+      if response.code != "200"
+        abort "There was an error requesting your IP address, IPAM returned code: #{response.code}, message: #{response.body}"
+      end
+      response_hash = JSON.parse(response.body)
+      puts response_hash["code"]
+      puts response_hash["data"]
+      new_ip = response_hash["data"]
+
+      uri = options[:add_uri]
+      uri = URI.escape(uri)
+      uri = URI.parse(uri.concat("addresses/?subnetId=#{subnetId}&ip=#{new_ip}&hostname=#{options[:hostname]}&owner=#{username}"))
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request.add_field("token",  auth_token)
+      response = http.request(request)
+      if response.code != "201"
+        abort "There was an error requesting your IP address, IPAM returned code: #{response.code}, message: #{response.body}"
+      end
+      response_hash = JSON.parse(response.body)
+      puts response.body
+    end
+    exit
+
+
 
       # Get an IP from our IPAM system
       uri = options[:add_uri].gsub(/SUBNET|HOSTNAME|USER|APIAPP|APITOKEN/, {
@@ -59,6 +138,6 @@ class Autoip < Plugin
       end
       options[:ip] = response.body
       puts "Assigned IP: #{options[:ip]}"
-    end
+#    end
   end
 end
